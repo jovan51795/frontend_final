608706d24125177fe3e059cd4f337b33d142e5d7
jovan51795 <62101107+jovan51795@users.noreply.github.com>
2022-10-06 14:05:28 +0800

    initial commit for student pages

--- lms/src/components/students/StudentsListTable.jsx
+++ lms/src/components/students/StudentsListTable.jsx
@@ -1,7 +1,19 @@
+import { CCard, CCardBody, CCardHeader, CCol, CRow, CTable } from '@coreui/react'
 import React from 'react'
 
 const StudentsListTable = () => {
-  return <div>StudentsListTable</div>
+  return (
+    <CRow>
+      <CCol xs={12}>
+        <CCard>
+          <CCardHeader></CCardHeader>
+          <CCardBody>
+            <CTable></CTable>
+          </CCardBody>
+        </CCard>
+      </CCol>
+    </CRow>
+  )
 }
 
 export default StudentsListTable
