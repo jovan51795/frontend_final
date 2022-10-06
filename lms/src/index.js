4939aecc7f330d777f7870e2dfbc2b534583aa11
jovan51795 <62101107+jovan51795@users.noreply.github.com>
2022-10-06 11:50:30 +0800

    remove unused components

--- lms/src/index.js
+++ lms/src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals'
 import { Provider } from 'react-redux'
 import store from './store'
 import 'react-toastify/dist/ReactToastify.css'
-import { BrowserRouter, HashRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <HashRouter>
