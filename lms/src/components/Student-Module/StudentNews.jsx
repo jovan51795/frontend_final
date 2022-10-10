e3254e240476d1cae660ee896161911e8f733daf
justin3ong <109696400+justin3ong@users.noreply.github.com>
2022-10-10 09:39:36 +0800

    `

---
+++ lms/src/components/Student-Module/StudentNews.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const StudentNews = () => {
+  return (
+    <div>
+      <img
+        src="https://www.liberty.edu/news/wp-content/uploads/sites/137/2019/12/Christmas-Tree-lighting-Liberty-campus-20131115LBmain.jpg"
+        alt="News pic"
+      />
+      <div>
+        <h2>This is a title</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. At non ex earum, libero
+          dignissimos voluptates. Quis beatae dolorem autem ipsa!
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default StudentNews
--- lms/src/views/students/StudentDashboard.jsx
+++ lms/src/views/students/StudentDashboard.jsx
@@ -20,11 +20,20 @@ import {
 } from '@coreui/react'
 import StudentDashboardWidgetsDropdown from 'src/components/Student-Module/StudentDashboardWidgets'
 import StudentDashboardCalendar from 'src/components/Student-Module/StudentDashboardCalendar'
+import StudentNews from 'src/components/Student-Module/StudentNews'
 
 const StudentDashboard = () => {
   return (
     <>
       <StudentDashboardWidgetsDropdown />
+      <CCard>
+        <CCardHeader>
+          <strong>University News</strong>
+        </CCardHeader>
+        <CCardBody>
+          <StudentNews />
+        </CCardBody>
+      </CCard>
       <CCard className="mb-12">
         <CCardHeader>
           <strong>Schedule</strong>
