ce5cd4e0280e4fba7369c34f1e912b9da3732df5
zione8036 <74102490+zione8036@users.noreply.github.com>
2022-10-20 11:57:36 +0800

    Professor graph

--- lms/src/views/charts/PassingBarChart.js
+++ lms/src/views/charts/PassingBarChart.js
@@ -1,7 +1,42 @@
 import React from 'react'
 import { CChart } from '@coreui/react-chartjs'
+import { getUserInfo } from 'src/services/userInfo'
+import {getPass, getFail, getConditional} from 'src/services/professorService'
+import { useState, useEffect } from 'react'
 
 const PassingBarChart = () => {
+  const [pass, setPass] = useState(null)
+  const [fail, setFail] = useState(null)
+  const [conditional, setConditonal] = useState(null)
+  const userInfo = getUserInfo().object
+  const getPasser = (id) => {
+      getPass(id).then((res) => {
+      if (res.data) {
+        setPass(res.data)
+      }
+    })
+  }
+  const getFails = (id) => {
+    getFail(id).then((res) => {
+    if (res.data) {
+      setFail(res.data)
+    }
+      })
+    }
+    const getConditionals = (id) => {
+      getConditional(id).then((res) => {
+      if (res.data) {
+        setConditonal(res.data)
+      }
+    })
+    }
+      useEffect(() => {
+        getPasser(userInfo.professor_id)
+        getFails(userInfo.professor_id)
+        getConditionals(userInfo.professor_id)
+      }, [])
+
+
   return (
     <div className="container">
       <CChart
@@ -12,7 +47,7 @@ const PassingBarChart = () => {
             {
               label: 'STUDENTS PASSING RATES',
               backgroundColor: '#2C8263',
-              data: [40, 20, 12],
+              data: [pass, fail, conditional],
             },
           ],
         }}
