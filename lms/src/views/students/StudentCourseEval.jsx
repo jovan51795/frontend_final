52cbaf077a51c575017f0d686f3c76697cb4f69e
justin3ong <109696400+justin3ong@users.noreply.github.com>
2022-10-22 20:52:43 +0800

    Update StudentCourseEval.jsx

--- lms/src/views/students/StudentCourseEval.jsx
+++ lms/src/views/students/StudentCourseEval.jsx
@@ -1,7 +1,54 @@
 import React from 'react'
+import {
+  CCard,
+  CCardHeader,
+  CCardBody,
+  CTable,
+  CTableHead,
+  CTableRow,
+  CTableHeaderCell,
+  CTableDataCell,
+  CTableBody,
+  CTableCaption,
+} from '@coreui/react'
 
 const StudentCourseEval = () => {
-  return <div>StudentCourseEval</div>
+  return (
+    <>
+      <CCard className="mb-4">
+        <CCardHeader>
+          <strong>Course Evaluation</strong>
+        </CCardHeader>
+        <CCardBody>
+          <CTable hover align="middle">
+            <CTableHead>
+              <CTableRow>
+                <CTableHeaderCell scope="col">Subject Code</CTableHeaderCell>
+                <CTableHeaderCell scope="col">Title</CTableHeaderCell>
+                <CTableHeaderCell scope="col">Professor</CTableHeaderCell>
+                <CTableHeaderCell scope="col">Units</CTableHeaderCell>
+              </CTableRow>
+            </CTableHead>
+            <CTableBody>
+              <CTableRow>
+                <CTableDataCell></CTableDataCell>
+                <CTableDataCell></CTableDataCell>
+                <CTableDataCell> </CTableDataCell>
+                <CTableDataCell></CTableDataCell>
+              </CTableRow>
+
+              <CTableRow>
+                <CTableDataCell colSpan={3} style={{ textAlign: 'right' }}>
+                  <b>Total Units:</b>
+                </CTableDataCell>
+                <CTableDataCell></CTableDataCell>
+              </CTableRow>
+            </CTableBody>
+          </CTable>
+        </CCardBody>
+      </CCard>
+    </>
+  )
 }
 
 export default StudentCourseEval
