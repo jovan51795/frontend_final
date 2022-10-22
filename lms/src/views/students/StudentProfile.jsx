138a588bad5cc0cc8dcefc9be1636f733a94946f
justin3ong <109696400+justin3ong@users.noreply.github.com>
2022-10-22 20:04:32 +0800

    Update StudentProfile.jsx

--- lms/src/views/students/StudentProfile.jsx
+++ lms/src/views/students/StudentProfile.jsx
@@ -4,9 +4,10 @@ import * as userInfoService from 'src/services/userInfo'
 
 const StudentProfile = () => {
   const student = userInfoService.getUserInfo().object
+  const type = student.type
   return (
     <div>
-      <StudentProfileForm studentInfo={student} />
+      <StudentProfileForm studentInfo={student} type={type} />
     </div>
   )
 }
