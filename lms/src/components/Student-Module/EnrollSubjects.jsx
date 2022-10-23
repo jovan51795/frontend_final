c9f1962e0b9fd850826e17d722c6e1fa6b8dfba4
justin3ong <109696400+justin3ong@users.noreply.github.com>
2022-10-24 03:04:09 +0800

    commit enrollment

--- lms/src/components/Student-Module/EnrollSubjects.jsx
+++ lms/src/components/Student-Module/EnrollSubjects.jsx
@@ -8,72 +8,70 @@ import {
   CTableHeaderCell,
   CTableDataCell,
   CTableBody,
-  CButton
+  CButton,
 } from '@coreui/react'
 import { Link } from 'react-router-dom'
 import React from 'react'
 import { getRecommendedSubject } from 'src/services/studentService'
-import { useState,  useEffect} from 'react'
+import { useState, useEffect } from 'react'
 import { getUserInfo } from 'src/services/userInfo'
 
 const EnrollSubjects = () => {
   const userInFo = getUserInfo().object
   const [subject, setSubject] = useState([])
   useEffect(() => {
-    getsubjects(userInFo.sem,userInFo.yearLevel,userInFo.course.courseId )
-    }, [])
-    const getsubjects =(x,y,z)=>{
-        getRecommendedSubject(x,y,z).then((res)=>{
-      if(res&&res.data){
+    getsubjects(userInFo.sem, userInFo.yearLevel, userInFo.course.courseId)
+  }, [])
+
+  const getsubjects = (x, y, z) => {
+    getRecommendedSubject(x, y, z).then((res) => {
+      if (res && res.data) {
         setSubject(res.data.object)
       }
-      })
-      }  
-      console.log(subject)
+    })
+  }
+  console.log(subject)
 
   return (
     <>
       <div>
-      <CCard className="mb-4">
-        <CCardHeader>
-          <strong>Recommended Subject</strong>
-        </CCardHeader>
-        <CCardBody style={{ oveflow: 'scroll', overflow: 'auto', height: '1000px' }}>
-          <CTable hover align="middle">
-            <CTableHead>
-              <CTableRow>
-                <CTableHeaderCell scope="col">Subject Code</CTableHeaderCell>
-                <CTableHeaderCell scope="col">Title</CTableHeaderCell>
-                <CTableHeaderCell scope="col">Units</CTableHeaderCell>
-                <CTableHeaderCell scope="col">Actions</CTableHeaderCell>
-              </CTableRow>
-            </CTableHead>
-            <CTableBody>
-            {subject.map((sub) => (
-                <CTableRow key={sub}>
-                  <CTableDataCell>{sub[6]}</CTableDataCell>
-                  <CTableDataCell>{sub[7]}</CTableDataCell>
-                  <CTableDataCell>{sub[8]}</CTableDataCell>
-                  <CTableDataCell>
-                  <CButton className="btn-primary">
-                  
-                <Link to={`/student/enroll/getschedule/${sub[0]}`} style={{ textDecoration: 'none' }}>
-                  <span style={{ color: 'white' }}>Enroll Subjects</span>
-                </Link>
-              </CButton>
-                  </CTableDataCell>
+        <CCard className="mb-4">
+          <CCardHeader>
+            <strong>Recommended Subject</strong>
+          </CCardHeader>
+          <CCardBody style={{ oveflow: 'scroll', overflow: 'auto', height: '1000px' }}>
+            <CTable hover align="middle">
+              <CTableHead>
+                <CTableRow>
+                  <CTableHeaderCell scope="col">Subject Code</CTableHeaderCell>
+                  <CTableHeaderCell scope="col">Title</CTableHeaderCell>
+                  <CTableHeaderCell scope="col">Units</CTableHeaderCell>
+                  <CTableHeaderCell scope="col">Actions</CTableHeaderCell>
                 </CTableRow>
-              ))}
-      
-
-              
-            </CTableBody>
-
-            
-          </CTable>
-        </CCardBody>
-      </CCard>
-    </div>
+              </CTableHead>
+              <CTableBody>
+                {subject.map((sub) => (
+                  <CTableRow key={sub}>
+                    <CTableDataCell>{sub[6]}</CTableDataCell>
+                    <CTableDataCell>{sub[7]}</CTableDataCell>
+                    <CTableDataCell>{sub[8]}</CTableDataCell>
+                    <CTableDataCell>
+                      <CButton className="btn-primary">
+                        <Link
+                          to={`/student/enroll/getschedule/${sub[0]}`}
+                          style={{ textDecoration: 'none' }}
+                        >
+                          <span style={{ color: 'white' }}>Enroll Subjects</span>
+                        </Link>
+                      </CButton>
+                    </CTableDataCell>
+                  </CTableRow>
+                ))}
+              </CTableBody>
+            </CTable>
+          </CCardBody>
+        </CCard>
+      </div>
     </>
   )
 }
--- lms/src/components/Student-Module/StudentSubjectSchedule.jsx
+++ lms/src/components/Student-Module/StudentSubjectSchedule.jsx
@@ -1,108 +1,120 @@
 import {
-    CCard,
-    CCardHeader,
-    CCardBody,
-    CTable,
-    CTableHead,
-    CTableRow,
-    CTableHeaderCell,
-    CTableDataCell,
-    CTableBody,
-    CButton
-  } from '@coreui/react'
-  import { Link } from 'react-router-dom'
-  import React from 'react'
-  import { getSchedule } from 'src/services/studentService'
-  import { useState,  useEffect} from 'react'
-  import { getUserInfo } from 'src/services/userInfo'
+  CCard,
+  CCardHeader,
+  CCardBody,
+  CTable,
+  CTableHead,
+  CTableRow,
+  CTableHeaderCell,
+  CTableDataCell,
+  CTableBody,
+  CButton,
+} from '@coreui/react'
+import { Link, useNavigate } from 'react-router-dom'
+import React from 'react'
+import { getSchedule } from 'src/services/studentService'
+import { useState, useEffect } from 'react'
+import { getUserInfo } from 'src/services/userInfo'
 import { useParams } from 'react-router-dom'
 import { getAllProfessor } from 'src/services/professorService'
+import { addSubject } from 'src/services/studentService'
+import { toast } from 'react-toastify'
 
 const StudentSubjectSchedule = () => {
-    const params = useParams()
-console.log(+params.id)
-
-
-const userInFo = getUserInfo()
-
-const [prof, setProf] = useState([])
-
-const getProf = ()=>{
-    getAllProfessor().then((res)=>{
-        if(res&&res.data){
-            setProf(res.data)
-          
-        }
-        })
-}
-
+  const params = useParams()
+  const userInFo = getUserInfo().object
+  const [prof, setProf] = useState([])
+  const navigate = useNavigate()
 
   const [schedule, setSchedule] = useState([])
 
   useEffect(() => {
     getsubjects(+params.id)
     getProf()
-    }, [])
-    const getsubjects =(id)=>{
-        getSchedule(id).then((res)=>{
-      if(res&&res.data){
+  }, [])
+
+  const getsubjects = (id) => {
+    getSchedule(id).then((res) => {
+      if (res && res.data) {
         setSchedule(res.data.object)
       }
-      })
-      }  
-    //   console.log(schedule)
-      console.log(prof.filter((x)=>{
-        x.professor_id = 1
-      }).map(x =>{
-        console.log(x)
-      }))
+    })
+  }
+
+  const getProf = () => {
+    getAllProfessor().then((res) => {
+      if (res && res.data) {
+        setProf(res.data)
+      }
+    })
+  }
+
+  const add = (studentID, subjectID) => {
+    addSubject(studentID, subjectID).then((res) => {
+      if (res && res.data && res.data.status === 1) {
+        toast.success(res.data.message)
+        // navigate('/student/enroll')
+      } else if (res && res.data && res.data.status === 0) {
+        toast.error(res.data.message)
+      }
+    })
+  }
+  const showProf = (data) => {
+    if (data != null && prof.length > 0) {
+      return (
+        prof.filter((x) => {
+          return x.professor_id === data
+        })[0].firstName +
+        ' ' +
+        prof.filter((x) => {
+          return x.professor_id === data
+        })[0].lastName
+      )
+    } else {
+      return ''
+    }
+  }
 
   return (
-<>
+    <>
       <div>
-      <CCard className="mb-4">
-        <CCardHeader>
-          <strong>Choose Schedule</strong>
-        </CCardHeader>
-        <CCardBody style={{ oveflow: 'scroll', overflow: 'auto', height: '1000px' }}>
-          <CTable hover align="middle">
-            <CTableHead>
-              <CTableRow>
-                <CTableHeaderCell scope="col">Time</CTableHeaderCell>
-                <CTableHeaderCell scope="col">Section</CTableHeaderCell>
-                <CTableHeaderCell scope="col">Start Date</CTableHeaderCell>
-                <CTableHeaderCell scope="col">Professor Name</CTableHeaderCell>
-                <CTableHeaderCell scope="col">Actions</CTableHeaderCell>
-              </CTableRow>
-            </CTableHead>
-            <CTableBody>
-             
-            {schedule.map((sched) => (
-                <CTableRow key={sched}>
-                  <CTableDataCell>{sched[3]}</CTableDataCell>
-                  <CTableDataCell>{sched[4]}</CTableDataCell>
-                  <CTableDataCell>{sched[5]}</CTableDataCell>
-                  <CTableDataCell></CTableDataCell>
-                  <CTableDataCell>
-                  <CButton className="btn-primary">
-                  
-                <Link to={`/student/enroll/getschedule/${sched[0]}`} style={{ textDecoration: 'none' }}>
-                  <span style={{ color: 'white' }}>Enroll Subjects</span>
-                </Link>
-              </CButton>
-                  </CTableDataCell>
+        <CCard className="mb-4">
+          <CCardHeader>
+            <strong>Choose Schedule</strong>
+          </CCardHeader>
+          <CCardBody style={{ oveflow: 'scroll', overflow: 'auto', height: '1000px' }}>
+            <CTable hover align="middle">
+              <CTableHead>
+                <CTableRow>
+                  <CTableHeaderCell scope="col">Time</CTableHeaderCell>
+                  <CTableHeaderCell scope="col">Section</CTableHeaderCell>
+                  <CTableHeaderCell scope="col">Start Date</CTableHeaderCell>
+                  <CTableHeaderCell scope="col">Professor Name</CTableHeaderCell>
+                  <CTableHeaderCell scope="col">Actions</CTableHeaderCell>
                 </CTableRow>
-              ))}
-      
-
-              
-            </CTableBody>
-
-            
-          </CTable>
-        </CCardBody>
-      </CCard>
-    </div>
+              </CTableHead>
+              <CTableBody>
+                {schedule.map((sched) => (
+                  <CTableRow key={sched}>
+                    <CTableDataCell>{sched[3]}</CTableDataCell>
+                    <CTableDataCell>{sched[4]}</CTableDataCell>
+                    <CTableDataCell>{sched[5]}</CTableDataCell>
+                    <CTableDataCell>{showProf(sched[7])}</CTableDataCell>
+                    <CTableDataCell>
+                      <CButton
+                        className="btn-primary"
+                        onClick={add(userInFo.student_id, +params.id)}
+                      >
+                        <span style={{ color: 'white' }}>Enroll Subjects</span>
+                      </CButton>
+                    </CTableDataCell>
+                  </CTableRow>
+                ))}
+              </CTableBody>
+            </CTable>
+          </CCardBody>
+        </CCard>
+      </div>
     </>
   )
 }
